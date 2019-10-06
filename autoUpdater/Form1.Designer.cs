edfc15171135aa2293643ccf25e749cdadbbef66
Furkan Cigerlioglu <furkan-34@users.noreply.github.com>
2019-10-06 12:21:05 +0300

    Add project files.

---
+++ autoUpdater/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace autoUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.versionNumber = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblNewest = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(94, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current Version Number:";
+            // 
+            // versionNumber
+            // 
+            this.versionNumber.AutoSize = true;
+            this.versionNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.versionNumber.Location = new System.Drawing.Point(276, 71);
+            this.versionNumber.Name = "versionNumber";
+            this.versionNumber.Size = new System.Drawing.Size(119, 16);
+            this.versionNumber.TabIndex = 1;
+            this.versionNumber.Text = "Version Number";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(558, 70);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(282, 134);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.Visible = false;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblLogin.Location = new System.Drawing.Point(177, 167);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(117, 17);
+            this.lblLogin.TabIndex = 3;
+            this.lblLogin.Text = "Login Situation";
+            // 
+            // lblNewest
+            // 
+            this.lblNewest.AutoSize = true;
+            this.lblNewest.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNewest.Location = new System.Drawing.Point(276, 117);
+            this.lblNewest.Name = "lblNewest";
+            this.lblNewest.Size = new System.Drawing.Size(119, 16);
+            this.lblNewest.TabIndex = 5;
+            this.lblNewest.Text = "Version Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(92, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Newest Version Number:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 267);
+            this.Controls.Add(this.lblNewest);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.versionNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Auto Updater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label versionNumber;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblNewest;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ autoUpdater/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Net;
+
+
+
+
+namespace autoUpdater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForUpdates();
+        }
+
+        public void CheckForUpdates()
+        {       // The link of version.txt at dropbox edited edited with dl=1 ends for auto download
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.dropbox.com/s/kto3l410r8kdb4e/version.txt?dl=1");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            System.IO.StreamReader sr = new System.IO.StreamReader(response.GetResponseStream());
+            string newestversion = sr.ReadToEnd();
+            string currentversion = Application.ProductVersion;
+            lblNewest.Text = newestversion;
+            if (newestversion.Contains(currentversion))
+            {
+                // You can change enables or visible settings of elements of login page.
+                lblLogin.Text = "Successfully logged in!";
+                versionNumber.Text = Application.ProductVersion;
+            }
+            else
+            {
+
+                MessageBox.Show("Please download the new version!\n(We advice you to download new version instead of open it.)");
+                // The link of exe file at dropbox edited with dl=1 ends for auto download
+                webBrowser1.Navigate("https://www.dropbox.com/s/fy6wgligohahs8p/autoUpdater.exe?dl=1");
+                lblLogin.Text = "Logging is failed!";
+                versionNumber.Text = Application.ProductVersion;
+            }
+
+        }
+
+
+    }
+}
